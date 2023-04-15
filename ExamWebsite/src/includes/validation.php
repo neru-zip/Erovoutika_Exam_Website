a5fb17d8896a65f1b251f98320e513e517b99277
JustMao21 <joshuacabanza@gmail.com>
2023-04-15 23:36:57 +0800

    validation bug fixed

--- ExamWebsite/src/includes/validation.php
+++ ExamWebsite/src/includes/validation.php
@@ -9,18 +9,24 @@ $password = $_POST['password'];
  * Simplify the fetched information by storing the data then passing it.
  * 
  */
-
-$result = mysqli_query($connectdb, "SELECT * FROM tbusers WHERE clUrUsername='$username' AND clUrPassword='$password' AND clUrLevel='0';");
+if (str_contains($username, '\'') || str_contains($username, '\"') || str_contains($password, '\'') || str_contains($password, '\"')){
+    echo "<script>
+        alert('Invalid username or password.');  
+        window.location = '../login.php';
+        </script>";
+}
+else{
+    $result = mysqli_query($connectdb, "SELECT * FROM tbusers WHERE clUrUsername='$username' AND clUrPassword='$password' AND clUrLevel='0';");
 while($row = mysqli_fetch_array($result)){
-	$success = true;
-	$clUrID = $row['clUrID'];
+    $success = true;
+    $clUrID = $row['clUrID'];
     $clUrUsername = $row['clUrUsername'];
-	$clUrFirstname = $row['clUrFirstname'];
+    $clUrFirstname = $row['clUrFirstname'];
 }
 
 if($success == true){
-	$_SESSION['admin_sid']=session_id();
-	$_SESSION['clUrID'] = $clUrID;
+    $_SESSION['admin_sid']=session_id();
+    $_SESSION['clUrID'] = $clUrID;
     $_SESSION['clUrUsername'] = $clUrUsername;
     session_start();
 
@@ -28,7 +34,7 @@ if($success == true){
     $sql = "UPDATE tbusers SET clUrLastLogin = date_format(now(), '%Y-%m-%d %H:%i:%s') WHERE clUrID = $clUrID;";
     $result = $connectdb->query($sql);
 
-	header("location: ../webadmin/AdminHome.php");
+    header("location: ../webadmin/AdminHome.php");
 }else{
         $result = mysqli_query($connectdb, "SELECT * FROM tbusers WHERE clUrUsername='$username' AND clUrPassword='$password' AND clUrLevel='1';");
         while($row = mysqli_fetch_array($result))
@@ -42,8 +48,8 @@ if($success == true){
         {
             $_SESSION['client_sid']=session_id();
             $_SESSION['clUrID'] = $clUrID;
-            $_SESSION['clUrLevel'] = $clUrLevel;	
-            session_start();	
+            $_SESSION['clUrLevel'] = $clUrLevel;    
+            session_start();    
 
             //------- Update latest login date
             $sql = "UPDATE tbusers SET clUrLastLogin = date_format(now(), '%Y-%m-%d %H:%i:%s') WHERE clUrID = $clUrID;";
@@ -53,9 +59,10 @@ if($success == true){
         }
         else
         {  echo "<script>
-			alert('Invalid username or password.');  
-			window.location = '../login.php';
-			</script>"; 
+            alert('Invalid username or password.');  
+            window.location = '../login.php';
+            </script>"; 
         }
+}
 }
 ?>
